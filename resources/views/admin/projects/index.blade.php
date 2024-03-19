9a6d3b4a635c52daf492afbf2c9cb4b00171207b
SantiGalvan <santiagogalvancolorado@gmail.com>
2024-03-19 23:32:28 +0100

    added index

---
+++ resources/views/admin/projects/index.blade.php
@@ -0,0 +1,46 @@
+@extends('layouts.app')
+
+@section('title', 'Projects')
+
+@section('content')
+    <section id="table-projects">
+        <h1 class="text-center my-4">Projects</h1>
+
+        <table class="table table-dark">
+            <thead>
+              <tr>
+                <th scope="col">#</th>
+                <th scope="col">Titolo</th>
+                <th scope="col">Linguaggio</th>
+                <th scope="col">Framework</th>
+                <th scope="col">Creato il</th>
+                <th scope="col">Ultima modifica</th>
+                <th scope="col"></th>
+              </tr>
+            </thead>
+            <tbody class="table-group-divider">
+                @forelse ($projects as $project)
+                <tr>
+                    <th scope="row">{{$project->id}}</th>
+                    <td>{{$project->title}}</td>
+                    <td>{{$project->language}}</td>
+                    <td>{{$project->framework}}</td>
+                    <td>{{$project->created_at}}</td>
+                    <td>{{$project->updated_at}}</td>
+                    <td>
+                        <div class="d-flex justify-content-end">
+                            <a href="{{route('admin.projects.show', $project->id)}}" class="btn btn-sm btn-primary"><i class="fas fa-eye"></i></a>
+                        </div>
+                    </td>
+                  </tr>
+                @empty
+                  <tr>
+                    <th colspan="7">
+                        <h3>Al momento non ci sono progetti</h3>
+                    </th>
+                </tr>  
+                @endforelse
+            </tbody>
+          </table>
+    </section>
+@endsection
