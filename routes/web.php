7052250d9045274e003c0481872a4b4eaf8714bf
SantiGalvan <santiagogalvancolorado@gmail.com>
2024-03-19 18:11:48 +0100

    added projects route

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\Admin\HomeController as AdminHomeController;
 use App\Http\Controllers\Guest\HomeController as GuestHomeController;
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\Admin\ProjectController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -19,8 +20,23 @@ use Illuminate\Support\Facades\Route;
 // Rotta home guest
 Route::get('/', GuestHomeController::class)->name('guest.home');
 
-// Rotta home admin
-Route::get('/admin', AdminHomeController::class)->middleware(['auth', 'verified'])->name('admin.home');
+
+Route::prefix('/admin')->name('admin.')->middleware('auth')->group(function () {
+    // Rotta Admin home
+    Route::get('', AdminHomeController::class)->name('home');
+
+    // Rotte Admin project CRUD
+    Route::get('/projects', [ProjectController::class, 'index'])->name('projects.index');
+    Route::get('/projects/create', [ProjectController::class, 'create'])->name('projects.create');
+    Route::get('/projects/{project}', [ProjectController::class, 'show'])->name('projects.show');
+    Route::post('/projects', [ProjectController::class, 'store'])->name('projects.store');
+    Route::get('/projects/{project}/edit', [ProjectController::class, 'edit'])->name('projects.edit');
+    Route::put('/projects/{project}', [ProjectController::class, 'update'])->name('projects.update');
+    Route::delete('/projects/{project}', [ProjectController::class, 'destroy'])->name('projects.destroy');
+
+    // Rotta Admin project CRUD generale
+    // Route::resource('projects', ProjectController::class);
+});
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
