b10c522304b9ce4370d8f16ec1ef61af0a711c0f
SantiGalvan <santiagogalvancolorado@gmail.com>
2024-03-21 00:07:51 +0100

    fixed guest home

--- resources/views/guest/home.blade.php
+++ resources/views/guest/home.blade.php
@@ -8,36 +8,43 @@
 <section id="projects-card">
     <div class="row mb-4">
         <h1 class="text-center my-4">Progetti</h1>
-        <div class="col">
         @forelse ($projects as $project)
-            <div class="card p-4 my-4">
+        <div class="col-4">
+            <div class="card my-4">
+                <div class="card-header text-center">
+                    <h3>{{$project->title}}</h3>
+                </div>
+                <div class="card-body text-center">
+                    <img src="{{$project->image}}" alt="{{$project->title}}" class="img-fluid mb-3">
+                    <div><strong>Linguaggio:</strong> {{$project->language}}</div>
+                    <div><strong>Framework:</strong> {{$project->framework}}</div>
+                    <div class="mt-2"><strong>Creato il:</strong> {{$project->created_at}}</div>
+                    <div><strong>Ultima modifica:</strong> {{$project->updated_at}}</div>
+                    <div class="mt-4"><a href="" class="btn btn-lg btn-primary"><i class="fa-solid fa-magnifying-glass me-2"></i>Vedi</a></div>
+                </div>
                 <div class="row">
-                    <div class="col-3">
-                        <img src="{{$project->image}}" alt="{{$project->title}}" class="img-fluid mb-3">
-                        <div><strong>Linguaggio:</strong> {{$project->language}}</div>
-                        <div><strong>Framework:</strong> {{$project->framework}}</div>
-                        <div class="mt-2"><strong>Creato il:</strong> {{$project->created_at}}</div>
-                        <div><strong>Ultima modifica:</strong> {{$project->updated_at}}</div>
-                    </div>
                     <div class="col">
-                        <h3>{{$project->title}}</h3>
-                        <p class="lead">{{$project->description}}</p>
+                        {{-- <p class="lead">{{$project->description}}</p> --}}
+                    </div>
+                    <div class="col-3">
                     </div>
                 </div>
             </div>
+        </div>
         @empty
-        <div class="card p-4 my-4">
-            <div class="row">
-                <h4 class="text-center my-4">Al momento non ci sono progetti</h4>
+        <div class="col-12">
+            <div class="card my-4">
+                <div class="card-body">
+                    <h4 class="text-center my-4">Al momento non ci sono progetti</h4>
+                </div>
             </div>
         </div>
         @endforelse
-        </div>
     </div>
 
     <!-- Pagination -->
     @if($projects->hasPages())
-    {{$projects->links()}}
-  @endif
+     {{$projects->links()}}
+    @endif
 </section>
 @endsection
