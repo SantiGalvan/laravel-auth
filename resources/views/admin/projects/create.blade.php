ca8f185f79858609b7c11931efeacef5d79b1a0a
SantiGalvan <santiagogalvancolorado@gmail.com>
2024-03-20 21:52:07 +0100

    added old & buttons

--- resources/views/admin/projects/create.blade.php
+++ resources/views/admin/projects/create.blade.php
@@ -14,36 +14,42 @@
                 <div class="col">
                     <div class="mb-3">
                         <label for="title" class="form-label">Titolo</label>
-                        <input type="text" class="form-control" id="title">
+                        <input value="{{old('title', '')}}" type="text" class="form-control" id="title">
                     </div>
                 </div>
                 <div class="col-3">
                     <div class="mb-3">
                         <label for="language" class="form-label">Linguaggio</label>
-                        <input type="email" class="form-control" id="language" name="language">
+                        <input value="{{old('language', '')}}" type="email" class="form-control" id="language" name="language">
                     </div>
                 </div>
                 <div class="col-3">
                     <div class="mb-3">
                         <label for="framework" class="form-label">Tecnologia Usata</label>
-                        <input type="email" class="form-control" id="framework" name="framework">
+                        <input value="{{old('framework', '')}}" type="email" class="form-control" id="framework" name="framework">
                     </div>
                 </div>
-                <div class="col-10">
+                <div class="col-11">
                     <div class="mb-3">
                         <label for="image" class="form-label">Immagine</label>
-                        <input type="email" class="form-control" id="image" name="image">
+                        <input value="{{old('image', '')}}" type="email" class="form-control" id="image" name="image">
                     </div>
                 </div>
-                <div class="col-2">
-                    <img src="" alt="" class="img-fluid" id="preview">
+                <div class="col-1">
+                    <div class="mb-3">
+                        <img src="https://marcolanci.it/boolean/assets/placeholder.png" alt="immagine del progetto" class="img-fluid" id="preview">
+                    </div>
                 </div>
-                <div class="col">
+                <div class="col-12">
                     <div class="mb-3">
                         <label for="description" class="form-label">Descrizione progetto</label>
-                        <textarea class="form-control" id="description" name="description" rows="10"></textarea>
+                        <textarea class="form-control" id="description" name="description" rows="10">{{old('description', '')}}</textarea>
                     </div>
                 </div>
+            </div>
+            <div class="d-flex justify-content-end align-items-center gap-3">
+                <button type="reset" class="btn btn-lg btn-warning"><i class="fas fa-arrows-rotate me-2"></i>Reset</button>
+                <button type="submit" class="btn btn-lg btn-success"><i class="fas fa-floppy-disk me-2"></i>Salva</button>
             </div>
         </form>
     </section>
