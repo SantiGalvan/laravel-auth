0e8ad5cf9bbabb2cdfefb5dde36bd713f8ccac68
SantiGalvan <santiagogalvancolorado@gmail.com>
2024-03-20 22:15:05 +0100

    added action edit

--- app/Http/Controllers/Admin/ProjectController.php
+++ app/Http/Controllers/Admin/ProjectController.php
@@ -56,7 +56,7 @@ class ProjectController extends Controller
      */
     public function edit(Project $project)
     {
-        //
+        return view('admin.projects.edit', compact('project'));
     }
 
     /**
